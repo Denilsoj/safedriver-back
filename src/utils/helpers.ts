17094c0686573c242d15c11d84b57f06e96e2315
Denilson Oliveira <juniod04@gmail.com>
2025-02-16 12:52:23 -0300

    feat: add feature for generate unique filename

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,3 +6,17 @@ export const isAdult = (date: string) => {
 
 	return age > 18;
 };
+
+export const generateUniqueFilename = (
+	fieldname: string,
+	filename: string,
+	driverName: string | unknown,
+): string => {
+	const uniqueSufix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+	const extension = filename.split(".").pop();
+	const driveNameTratament =
+		typeof driverName === "string"
+			? driverName.replaceAll(" ", "_")
+			: driverName;
+	return `${fieldname}-${driveNameTratament}-${uniqueSufix}.${extension}`;
+};

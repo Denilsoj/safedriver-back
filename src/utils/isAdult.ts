c1a713cb6f86ffed2976f0a9bc1cb12d322baa42
Denilson Oliveira <juniod04@gmail.com>
2025-02-24 14:32:56 -0300

    fix: alter name files

--- src/utils/isAdult.ts
+++
@@ -1,8 +0,0 @@
-export const isAdult = (date: string) => {
-	const birth_date = new Date(date);
-	const today = new Date();
-
-	const age = today.getFullYear() - birth_date.getFullYear();
-
-	return age > 18;
-};

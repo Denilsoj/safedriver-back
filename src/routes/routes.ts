6f5ae9517ab3c5bdd4eb826c8552b3b35811ea93
Denilson Oliveira <juniod04@gmail.com>
2025-02-24 00:24:19 -0300

    feat: add route for update drivers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,9 @@
 import DriverController from "../controllers/DriverController";
 import type { FastifyTypedInstance } from "../types/global";
-import { CreateDriverSchema } from "../models/DriverModel";
-import { z } from "zod";
+
 export async function routes(app: FastifyTypedInstance) {
 	app.get("/driver", DriverController.show);
 	app.post("/driver", DriverController.store);
+	app.get("/driver/:cpf", DriverController.index);
+	app.patch("/driver/:cpf", DriverController.update);
 }
